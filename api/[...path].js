0793ae1893c28f6caf348d0d58e30a3da88c72ad
raspmanzzang <raspman.zzang@gmail.com>
2025-10-10 16:13:37 +0900

    Update [...path].js

--- api/[...path].js
+++ api/[...path].js
@@ -85,7 +85,7 @@ export default async function handler(req, res) {
     
     try {
         const pathArray = req.query.path || [];
-        const filePath = decodeURIComponent(pathArray.join('/')); // URL 디코딩
+        const filePath = decodeURIComponent(pathArray.join('/'));
         console.log('Raw path:', req.query.path, 'Parsed filePath:', filePath);
         
         if (!filePath || !filePath.includes('/')) {
